43e2c45b65a8957c49819cd0d58118aa326efaa3
Steve Leonard <batman.xx@gmail.com>
2012-11-18 10:34:49 -0800

    Update setup.py
    rm email

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(name='Text2image',
       version='0.1',
       description='Convert text to an image',
       author='Steve Leonard',
-      author_email='sleonard76@gmail.com',
       url='https://github.com/xsleonard/text2image',
       py_modules=['text2image'],
      )
